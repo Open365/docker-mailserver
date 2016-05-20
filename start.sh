c83255d8a6fbbb356403f0755f97a2e1c581c121
Adrián Norte <adrian@bashlines.com>
2016-05-20 08:37:13 +0200

    Removing the logic of multitenant.
    We no longer have the option to disable multitenancy so we use the domain on the user path.

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 eyeos-service-ready-notify-cli &
-if [ $EYEOS_USE_MULTITENANT == "true" ]
-then
-    mailPath=maildir:/mnt/rawFS/users/%d/%n/mailbox:LAYOUT=fs
-else
-    mailPath=maildir:/mnt/rawFS/users/%n/mailbox:LAYOUT=fs
-fi
+mailPath=maildir:/mnt/rawFS/users/%d/%n/mailbox:LAYOUT=fs
 
 sed -i 's@LDAPHOST@'$EYEOS_MAILSERVER_LDAP_URL'@g' /etc/dovecot/auth-ldap.conf.ext
 sed -i 's@MANAGERDN@'$EYEOS_MAILSERVER_MANAGER_DN'@g' /etc/dovecot/auth-ldap.conf.ext
