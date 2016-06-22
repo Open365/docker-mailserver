1cb22d0affbb8fb5c9960f7bc221a6c4ab10556f
Carlos Campderrós <carlos.campderros@eyeos.com>
2016-06-22 13:16:40 +0200

    stop services.sh correctly when receiving SIGTERM

--- services.sh
+++ services.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+finish_everything() {
+	echo "Stopping everything..." >&2
+	set -x
+	/usr/sbin/postfix stop
+	dovecot stop
+	pgrep rsyslogd | xargs -r kill
+	pgrep postgrey | xargs -r kill
+
+	kill "$TAILPID"
+}
+
+trap finish_everything TERM INT
+
 /usr/sbin/rsyslogd
 /usr/sbin/postgrey \
 	--inet=127.0.0.1:10030 \
@@ -8,4 +21,13 @@
 	--greylist-text="Greylisted for %s seconds"
 /usr/sbin/postfix start
 /usr/sbin/dovecot
-tail -F /var/log/maillog
+tail -F /var/log/maillog &
+TAILPID="$!"
+
+# wait causes this script to block until a background process finishes.
+# The only background process here is tail -F, which will never finish.
+# We do this `wait` thing instead of leaving `tail -F` in the foreground
+# because wait is a builtin command and it processes signals, while if we are
+# running an external command we won't process signals until the command
+# returns
+wait
